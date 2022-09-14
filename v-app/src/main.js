25eda517da460f73a82e1271eb05a65f76df403d
mukund <mukund@utilizecore.com>
2022-09-13 21:13:26 -0600

    added react and vue app to container

---
+++ v-app/src/main.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import wrap from "@vue/web-component-wrapper";
+
+import App from "./App.vue";
+
+const wrappedElement = wrap(Vue, App);
+
+window.customElements.define("vue-app", wrappedElement);
